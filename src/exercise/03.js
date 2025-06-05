283c31b195561d335960bd462785563ad7805fa6
Rayan Mejaat <142797698+HandiReaper@users.noreply.github.com>
2025-06-05 23:55:44 +0200

    Exercice 3 & Bonus 1

--- src/exercise/03.js
+++ src/exercise/03.js
@@ -2,58 +2,57 @@
 // Lifting state
 // http://localhost:3000/alone/exercise/03.js
 
-import * as React from 'react'
+import * as React from "react"
 
 // 👨‍✈️ Nous voulons afficher dans le composant Content, l'ordinateur préféré
 
 // 🐶 accepte `computer` et `onComputerChange` en props de ce composant
-function MyBestComputer() {
+function MyBestComputer({ computer, onComputerChange }) {
   // ⛏️ supprime ce state il sera géré dans le composant parent.
-  const [computer, setComputer] = React.useState('MacBookPro')
+
   return (
     <div>
       <label>Mon ordinateur préféré : </label>
       <input
         value={computer}
         // 🐶 remplace `setComputer` par `onComputerChange`
-        onChange={event => setComputer(event.target.value)}
+        onChange={(event) => onComputerChange(event.target.value)}
       />
     </div>
   )
 }
 
-function UserName({userName, onUserNameChange}) {
+function UserName() {
+  const [userName, setUserName] = React.useState("")
   return (
     <div>
       <label>Nom d'utilisateur : </label>
       <input
         value={userName}
-        onChange={event => onUserNameChange(event.target.value)}
+        onChange={(event) => setUserName(event.target.value)}
       />
     </div>
   )
 }
 
 // 🐶 accepte `computer` en props
-function Content({userName}) {
+function Content({ computer }) {
   return (
     <div>
-      {/* ⛏️ surprime ce libellé */}
-      Salut <b>{userName}</b> !{/* 🐶 Utilise plutôt celui ci */}
-      {/* Salut <b>{userName}</b>, ton ordinateur préféré est <b>{computer}</b> */}
+      Salut, ton ordinateur préféré est <b>{computer}</b>
     </div>
   )
 }
 function App() {
   // 🐶 ajoute useState 'computer'
-  const [userName, setUserName] = React.useState('')
+  const [computer, setComputer] = React.useState("MacBookPro")
   return (
     <div>
       {/* 🐶 passe computer et onComputerChange en props */}
-      <MyBestComputer />
-      <UserName userName={userName} onUserNameChange={setUserName} />
+      <MyBestComputer computer={computer} onComputerChange={setComputer} />
+      <UserName />
       {/* 🐶 passe le prop computer ici */}
-      <Content userName={userName} />
+      <Content computer={computer} />
     </div>
   )
 }
